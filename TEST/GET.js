b32f7d6e79a0e0dfe9d6995319c6a3c0f5616cd8
KennRobot <kebrgonzalezdo@ittepic.edu.mx>
2025-03-19 17:54:17 -0700

    Creacion de proyecto
    subida de proyecto, con las dependencias y tambien el Get ALL funcionando bien

---
+++ TEST/GET.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const API_URL = "https://ejercicio-apirest-nodejs.onrender.com/api/publication";
+
+// Obtener todas las publicaciones
+const getPublications = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    console.log("📌 Publicaciones:", response.data);
+  } catch (error) {
+    console.error("❌ Error al obtener publicaciones:", error.response?.data || error.message);
+  }
+};
+
+// Ejecutar pruebas
+getPublications();
